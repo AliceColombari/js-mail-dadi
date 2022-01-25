89a0bac2a77c65ae9b725a654d599dfa6b0c9cfb
AliceColombari <colombari.23@gmail.com>
2022-01-25 14:05:14 +0100

    esercizio uno: email, completato

---
+++ Mail/main.js
@@ -0,0 +1,42 @@
+// MAIL
+
+// chiedi all'utente la sua email
+// controlla che sia nella lista di chi può accedere
+// stampa un messaggio appropriato sull'esito di controllo
+
+// ALERT BENVENUTO
+alert('Benvenuto!')
+
+// CREAZIONE LISTA EMAIL
+const listaEmail = ['colombari.23@gmail.com', 'fedor1998@icloud.com', 'anonymous@gmail.com'];
+console.log(listaEmail);
+
+// CHIEDO ALL'UTENTE L'EMAIL
+const emailUtente = prompt('Digita la tua email per verificare se sei tra i partecipanti ')
+console.log(emailUtente);
+
+// EMAIL
+var emailPresente = false;
+
+// VERIFICO LA PRESENZA DELLA EMAIL UTENTE NELLA LISTA EMAIL
+for (let i=0; i < listaEmail.length; i++) {
+    console.log(i);
+
+    if (listaEmail[i] == emailUtente) {
+        emailPresente = true;
+        console.log(emailPresente);
+    }
+}
+
+// COMUNICO ALL'UTENTE SE PUO ACCEDERE
+if (emailPresente == false) {
+    console.log('email non presente');
+
+    alert('Non sei autorizzato ad accedere!');
+    document.querySelector('.title').innerHTML = 'Non sei autorizzato ad accedere!';
+
+} else {
+    console.log('email presente')
+
+    alert('Sei autorizzato ad accedere!');
+}
